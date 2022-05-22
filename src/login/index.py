0c3768db35e8d8cafef21d358d0a3728dd427e22
Joan Boulangger <joan.boulangger@gmail.com>
2022-05-22 09:21:32 -0500

    Protected routes JWT

--- src/login/index.py
+++ src/login/index.py
@@ -1,9 +1,15 @@
-from colorama import Cursor
-from flask import Flask, jsonify, request, session, jsonify
+from flask import Flask, jsonify, request, jsonify
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager, set_access_cookies, unset_jwt_cookies, get_jwt
 from werkzeug.security import check_password_hash
 import pyodbc
+from datetime import timedelta, datetime, timezone
 
 app = Flask (__name__)
+jwt = JWTManager (app)
+app.config["JWT_SECRET_KEY"] = "super-scret"
+app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
 driver = 'SQL Server Native Client 11.0'
 server = 'localhost'
@@ -15,8 +21,22 @@ try:
         driver, server, database, trusted_connection
     ))
     cursor = connection.cursor()
-except Exception as ex:
-    print (ex)
+    print('Connect!')
+
+except Exception as exception:
+    print (exception)
+
+def refresh_expiring_jwts(response): 
+    try: 
+        exp_timestamp = get_jwt()['exp']
+        now = datetime.now (timezone.utc)
+        target_timestamp = datetime.timestamp (now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp: 
+            access_token = create_access_token (identity=get_jwt_identity())
+            set_access_cookies (response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        return response
 
 
 @app.route ("/test")
@@ -25,36 +45,43 @@ def test():
 
 @app.route ("/")
 def home():
-    if 'username' in session:
-        username = session['username']
-        return jsonify (msg = 'You are already logged in', username = username)
+    pass
 
-    else:
-        resp = jsonify (msg = 'Unauthorized')
-        resp.status_code = 401
-        return resp
-
-
-@app.route ("/login", methods=['POST', 'GET'])
+@app.route ("/login", methods=['POST'])
 def login():
-    if request.method == 'GET': 
-        return jsonify (msg = 'Hola')
+   
     request_data = request.get_json()
     username = request_data['username']
     password = request_data['password']
-    cursor.execute ('SELECT USERNAME, PASSWORD FROM USERS')
+    cursor.execute ("SELECT USERNAME, PASSWORD FROM USERS WHERE USERNAME = ?", username)
     row = cursor.fetchone()
     db_user = row[0]
     db_pass = row[1]
     if username == db_user:
         verify = check_password_hash (db_pass, password)
         if verify: 
-            return jsonify (msg = 'Logged')
+            msg = jsonify ({'msg' : 'Logged'})
+            access_token = create_access_token (identity = username)
+            print (access_token)
+            set_access_cookies (msg,access_token)
+            return msg
+            
         else: 
             return jsonify (msg = 'Failed password')
     else: 
         return jsonify (msg = 'Failed username or password')
     
+@app.route ("/logout", methods = ["POST"])
+def logout ():
+    response = jsonify (msg = 'Loggout')
+    unset_jwt_cookies (response)
+    return response
+
+
+@app.route ("/protected")
+@jwt_required()
+def protected ():
+    return jsonify (foo="bar")
 
 if __name__ == '__main__': 
     app.debug = True
